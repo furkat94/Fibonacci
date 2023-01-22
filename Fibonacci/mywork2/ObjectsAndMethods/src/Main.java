af8b84beb408701db15a7ee6da54ed7469bb7dc8
furkat94 <104371098+furkat94@users.noreply.github.com>
2023-01-22 17:23:11 +0300

    Delete Fibonacci/mywork2 directory

--- Fibonacci/mywork2/ObjectsAndMethods/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Basket basket = new Basket();
-        basket.add("Milk", 40, 1, 1);
-        basket.add("Apple", 100, 1.5);
-        basket.print("Список товаров:");
-    }
-}
