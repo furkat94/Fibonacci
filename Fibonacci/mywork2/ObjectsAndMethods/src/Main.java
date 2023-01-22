9795ed3690b3556953cc6815933389e15253649f
furkat94 <104371098+furkat94@users.noreply.github.com>
2023-01-22 17:21:00 +0300

    Add files via upload

---
+++ Fibonacci/mywork2/ObjectsAndMethods/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Basket basket = new Basket();
+        basket.add("Milk", 40, 1, 1);
+        basket.add("Apple", 100, 1.5);
+        basket.print("Список товаров:");
+    }
+}
